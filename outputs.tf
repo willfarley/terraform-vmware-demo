ba9b684c1b070257e6cfb18fbd62d92135511e47
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2020-04-02 19:51:24 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,14 @@ output "ResPool_ID" {
   value       = "${module.vmware_linux_db_vm.respool_id}"
 }
 
-output "vm_name" {
-  description = "VMs names deployed from all resources"
-  value       = ["${module.vmware_linux_web_vm.vm_name}"]
-  value       = ["${module.vmware_linux_db_vm.vm_name}"]
-}
-
-output "vm_ip" {
-  description = "VMs IPs deployed from resource LinuxVM"
-  value       = ["${module.vmware_linux_db_vm.vm_ip}"]
-  value       = ["${module.vmware_linux_web_vm.vm_ip}"]
-}
+#output "vm_name" {
+#  description = "VMs names deployed from all resources"
+#  value       = ["${module.vmware_linux_web_vm.vm_name}"]
+#  value       = ["${module.vmware_linux_db_vm.vm_name}"]
+#}
+#
+#output "vm_ip" {
+#  description = "VMs IPs deployed from resource LinuxVM"
+#  value       = ["${module.vmware_linux_db_vm.vm_ip}"]
+#  value       = ["${module.vmware_linux_web_vm.vm_ip}"]
+#}
